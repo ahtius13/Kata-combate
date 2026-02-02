04694a7a52cbdd15bd710d3675d9bbebdc85e87d
ahtius13 <ahtius13@gmail.com>
2026-02-02 18:09:28 +0100

    Add files via upload

--- luchador.py
+++ luchador.py
@@ -3,14 +3,35 @@ import random
 class Luchador:
     
 
-    def __init__(self, nombre, vida, fuerza, resistencia, agilidad):
+    def __init__(self, nombre, tipo,  vida, fuerza, resistencia, agilidad):
 
         self.nombre = nombre
+        self.tipo = tipo 
         self.vida = int(vida)
         self.fuerza = int(fuerza)
         self.resistencia = int(resistencia)
         self.agilidad = int(agilidad)
 
+    def preparar (self):
+        if self.tipo == "ataque":
+            self.vida = self.vida + random.randint (0, 20)
+            self.fuerza = self.fuerza + random.randint (0, 50)
+            self.resistencia = self.resistencia + random.randint (0, 10)
+            self.agilidad = self.agilidad + random.randint (0, 30)
+        elif self.tipo == "defensa":
+            self.vida = self.vida + random.randint (0, 30)
+            self.fuerza = self.fuerza + random.randint (0, 10)
+            self.resistencia = self.resistencia + random.randint (0, 50)
+            self.agilidad = self.agilidad + random.randint (0, 20)
+        elif self.tipo == "agilidad":
+            self.vida = self.vida + random.randint (0, 10)
+            self.fuerza = self.fuerza + random.randint (0, 30)
+            self.resistencia = self.resistencia + random.randint (0, 20)
+            self.agilidad = self.agilidad + random.randint (0, 50)
+        else:
+           return print ("El tipo de su luchador no existe")
+        
+        return self.vida, self.fuerza, self.resistencia, self.agilidad
 
     def atacar (self):
          dados = self.fuerza / 10
--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 from luchador import Luchador
 import random
 
-ninja = Luchador ("Hattori", random.randrange (10,300,10),random.randrange (10,300,10), random.randrange (10,300,10), random.randrange (10,300,10) )
+ninja = Luchador ("Hattori", "agilidad", 100, 30, 30, 30)
 
-tercio = Luchador ("Santiago", random.randrange (10,300,10),random.randrange (10,300,10), random.randrange (10,300,10), random.randrange (10,300,10) )
+tercio = Luchador ("Santiago", "fuerza", 100, 30, 30, 30 )
 
+ninja.preparar
+tercio.preparar
 
-turno = 1
+if __name__ == "__main__":
 
-while ninja.vida > 0 & tercio.vida > 0:
-    if ninja.iniciativa > tercio.iniciativa:
-        ninja.atacar
-        tercio.defender
-        print (f"Turno {turno}")
-        print (f"{ninja.nombre} ataca a {tercio.nombre} cuya vida restante es {tercio.lifeLost} ")
-        turno =+ 1
-    else:
-        tercio.atacar
-        ninja.defender
-        print (f"Turno {turno}")
-        print (f"{tercio.nombre} ataca a {ninja.nombre} cuya vida restante es {ninja.lifeLost} ")
-        turno =+ 1
+    turno = 1
 
-if ninja.vida <= 0:
-    print (f"¡{ninja.nombre} ha sido derrotado, {tercio.nombre} gana!")
+    while ninja.vida > 0 & tercio.vida > 0:
+        if ninja.iniciativa > tercio.iniciativa:
+            ninja.atacar
+            tercio.defender
+            print (f"Turno {turno}")
+            print (f"{ninja.nombre} ataca a {tercio.nombre} cuya vida restante es {tercio.lifeLost} ")
+            turno =+ 1
+        else:
+            tercio.atacar
+            ninja.defender
+            print (f"Turno {turno}")
+            print (f"{tercio.nombre} ataca a {ninja.nombre} cuya vida restante es {ninja.lifeLost} ")
+            turno =+ 1
 
-if tercio.vida <= 0:
-    print (f"¡{tercio.nombre} ha sido derrotado, {ninja.nombre} gana!")
+    if ninja.vida <= 0:
+        print (f"¡{ninja.nombre} ha sido derrotado, {tercio.nombre} gana!")
+
+    if tercio.vida <= 0:
+        print (f"¡{tercio.nombre} ha sido derrotado, {ninja.nombre} gana!")
